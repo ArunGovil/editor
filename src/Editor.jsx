65f0d82b3228fa5ffceab4c4be7602cd527e9f65
ArunGovil <a.arungovil@gmail.com>
2023-07-14 08:36:34 +0530

    add linkify to support autolink

--- src/Editor.jsx
+++ src/Editor.jsx
@@ -1,6 +1,7 @@
 import { useEditor, EditorContent } from "@tiptap/react";
 import StarterKit from "@tiptap/starter-kit";
 import Link from "@tiptap/extension-link";
+import linkifyHtml from "linkify-html";
 
 const Editor = () => {
   const editor = useEditor({
@@ -10,10 +11,18 @@ const Editor = () => {
         HTMLAttributes: {
           target: "_blank",
         },
+        after: false,
       }),
     ],
     content: "",
     autofocus: true,
+    onUpdate({ editor }) {
+      const content = editor.getHTML();
+      const linkedContent = linkifyHtml(content);
+      if (content !== linkedContent) {
+        editor.commands.setContent(linkedContent);
+      }
+    },
   });
 
   return <EditorContent editor={editor} />;
